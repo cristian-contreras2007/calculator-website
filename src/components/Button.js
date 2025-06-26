e23c27a32729f019ff62e512c628f766c7f97a2b
Joe Mama <cristianrcontreras2007@gmail.com>
2025-06-25 21:44:52 -0700

    Commit

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import "./Button.css";
+
+const Button = ({symbol, color, handleClick}) => {
+    return (
+        <div className="button-wrapper" style={{ backgroundColor: color }}
+        onClick={() => handleClick(symbol)}>
+            {symbol}
+        </div>
+    )
+}
+
+export default Button;
